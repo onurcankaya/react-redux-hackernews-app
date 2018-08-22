46e2b7bc2455b1aa0f86bbae6baabcb187d06a8d
onurcankaya <onur.eren.cankaya@gmail.com>
2018-08-22 15:01:07 +0200

    changed the redux file structure

---
+++ src/store/actions.js
@@ -0,0 +1,6 @@
+const archiveStory = (id) => ({
+  type: 'ARCHIVE_STORY',
+  id,
+})
+
+export default archiveStory
--- src/store/reducers/archive.js
+++ src/store/reducers/archive.js
@@ -1,10 +1,8 @@
-import { ARCHIVE_STORY } from '../constants/action-types'
-
 const INITIAL_STATE = []
 
 const archiveReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case ARCHIVE_STORY:
+    case 'ARCHIVE_STORY':
       return [...state, action.id]
     default:
       return state
